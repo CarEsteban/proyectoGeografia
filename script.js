6447420dbbb3c3fcf26ae4fe6c7da24837a9538f
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2023-05-13 01:18:11 -0600

    Boton, ventana emergente, correciones lol
    Agrege la ventana emergente al presionar un boton, agrege un archivo js para funcionalidades... Buen resumen pero aun falta mejorar ciertas cosas. Pensaba poner una imagen de fondo en la ventana emergente (por eso esta el archivo brujulaMenu.jpeg)

---
+++ script.js
@@ -0,0 +1,6 @@
+var boton = document.getElementById("Botonjugar");
+const ventanaEmergente = document.getElementById('VentanaEmergente');
+
+boton.addEventListener("click", function() {
+    ventanaEmergente.style.display = 'block';
+});
