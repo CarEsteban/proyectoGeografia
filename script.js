f62d1ddc2508b0f97fd1186e225ab0675ad76c9b
CarEsteban <car23016@uvg.edu.gt>
2023-05-21 16:00:30 -0600

    Add other screens
    El resto de ventanas fueron añadidas y modificado el código js para optimizarlo y hacer que funcione de una mejor forma mediante bucles

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-//este codigo funciona con el pin no.5
-//para los otros deberías te doy dos opciones que se me vienen a la mente
-// 1. escribir una ventana para cada pregunta
-// 2. intentar ver como pasar parametros y escribir una funcion 
-//que haga lo mismo y que los parametros cambien la pregunta y 
-//las respuestas JAJAJ
-
-// suerte a quien le toque hacer lo de las preguntas <3
-
-var boton = document.getElementById("quintaPregunta");
-const ventanaEmergente = document.getElementById('VentanaEmergente');
-
-var closeButton = document.getElementById("closeButton");
-
-boton.addEventListener("click", function() {
-    ventanaEmergente.style.display = 'block';
-});
-closeButton.addEventListener("click", function() {
-    ventanaEmergente.style.display = 'none';
-});
-
-
-
-
-
-
-
-//CÓDIGO PARA PUNTOS
 
 //Definición de variables
 
+//Varibales de los botones (pines) y para cerrar
+var botones = document.querySelectorAll(".pin");
+var closeButtons = document.querySelectorAll(".closeButton");
+
+//variables para las ventanas
+var ventanaUno = document.getElementById('preguntaUno');
+var ventanaDos = document.getElementById('preguntaDos');
+var ventanaTres = document.getElementById('preguntaTres');
+var ventanaCuatro = document.getElementById('preguntaCuatro');
+var ventanaCinco = document.getElementById('preguntaCinco');
+var ventanaSeis = document.getElementById('preguntaSeis');
+var ventanaSiete = document.getElementById('preguntaSiete');
+var ventanaOcho = document.getElementById('preguntaOcho');
+
+var ventanasEmergentes = [ventanaUno,ventanaDos,ventanaTres,ventanaCuatro,ventanaCinco,ventanaSeis,ventanaSiete,ventanaOcho]
+
+//Variables para sumar los puntos
 var botonesSumarPuntos = document.querySelectorAll(".btnPuntos")
 var punteosASumar = [];
 
-
-// Agregar evento "click" a los botones para guardar los valores en el array
-botonesSumarPuntos.forEach(function(boton) {
-    boton.addEventListener('click', function() {
-      var valorSeleccionado = boton.value;
-      punteosASumar.push(valorSeleccionado);
-    });
+//Para poder abrir las ventanas emergentes
+Array.from(botones).forEach(function(boton) {
+  boton.addEventListener("click", function() {
+    var indice = Array.from(botones).indexOf(boton);
+    ventanasEmergentes[indice].style.display = 'block';
   });
-  
-  // Evento "click" en el botón closeButton para sumar los valores del array
-  closeButton.addEventListener('click', function() {
+});
+
+Array.from(closeButtons).forEach(function(boton) {
+  boton.addEventListener("click", function() {
+    var indice = Array.from(closeButtons).indexOf(boton);
+    ventanasEmergentes[indice].style.display = 'none';
+
+    //código para los puntos
     var suma = 0;
     punteosASumar.forEach(function(valor) {
       suma += parseInt(valor);
     });
     document.querySelector("#contadorPuntos").innerHTML=suma
-    // Resto del código para mostrar o utilizar la suma de los valores
+    
   });
+});
+
+
+botonesSumarPuntos.forEach(function(boton) {
+  boton.addEventListener('click', function() {
+    var valorSeleccionado = boton.value;
+    punteosASumar.push(valorSeleccionado);
+  });
+});
+  
