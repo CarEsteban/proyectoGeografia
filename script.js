569ceaaf39fb9b8d2eea4c3df178ffd2cb1ddb0e
CarEsteban <car23016@uvg.edu.gt>
2023-05-20 20:04:38 -0600

    Code Adapted
    Codigo adaptado para poder ejecutar las ventanas de cada pregunta.
    
    Pre estructura para poder hacer la asignación de los puntos

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var boton = document.getElementById("Botonjugar");
+//este codigo funciona con el pin no.5
+//para los otros deberías te doy dos opciones que se me vienen a la mente
+// 1. escribir una ventana para cada pregunta
+// 2. intentar ver como pasar parametros y escribir una funcion 
+//que haga lo mismo y que los parametros cambien la pregunta y 
+//las respuestas JAJAJ
+
+// suerte a quien le toque hacer lo de las preguntas <3
+
+var boton = document.getElementById("quintaPregunta");
 const ventanaEmergente = document.getElementById('VentanaEmergente');
 
 boton.addEventListener("click", function() {
